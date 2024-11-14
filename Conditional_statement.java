46f3209091d6432634d42e7513bc0f8bd3fb1cb8
Gokul200318 <165988049+Gokul200318@users.noreply.github.com>
2024-11-14 15:21:02 +0530

    if statement

---
+++ Conditional_statement.java
@@ -0,0 +1,11 @@
+class Conditional_statement{
+    public static void main(String[] args) {
+        int a = 18;
+        if (a>=18){
+            System.out.println("adult");
+        }
+            else{
+                System.out.println("child");
+            }
+        }
+    }
